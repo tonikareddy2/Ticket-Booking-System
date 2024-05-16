fece1f5ddfd38a7f8471a6a24198163a93d535a0
tonikareddy2 <tonikareddy2@gmail.com>
2024-05-16 16:08:20 +0530

    database connection

---
+++ Util/DBconn.py
@@ -0,0 +1,13 @@
+import pyodbc
+from Util.DBPropertyUtil import PropertyUtil
+
+
+class DBconnection:
+    def __init__(self):
+        conn_str = PropertyUtil.get_property_string()
+        self.conn = pyodbc.connect(conn_str)
+        self.cursor = self.conn.cursor()
+
+    def close(self):
+        self.cursor.close()
+        self.conn.close()
